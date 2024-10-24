9d8b0682f98fc6d650aed3be8310cc0dd2238edf
@0xb17bea125 <t@mail.bitbears.dev>
2024-10-24 21:48:58 +0900

    improve how to render lambda details

--- src/fonts.rs
+++ src/fonts.rs
@@ -8,3 +8,12 @@ pub fn get_default_font() -> Font {
         style: iced::font::Style::Normal,
     }
 }
+
+pub fn get_bold_font() -> Font {
+    Font {
+        family: iced::font::Family::Name("Noto Sans Mono"),
+        weight: iced::font::Weight::Bold,
+        stretch: iced::font::Stretch::Normal,
+        style: iced::font::Style::Normal,
+    }
+}
--- src/resource_details/lambda_function_details.rs
+++ src/resource_details/lambda_function_details.rs
@@ -1,11 +1,13 @@
 use aws_config::{BehaviorVersion, Region};
 use iced::{
-    widget::{checkbox, column, row, text, text_input},
+    alignment::{Horizontal, Vertical},
+    widget::{column, row, scrollable, text, text_input},
     Element, Length,
 };
 
 use crate::{
     error::Error,
+    fonts,
     message::Message,
     resource::{LambdaFunctionInfo, Resource},
     service::Service,
@@ -71,72 +73,74 @@ impl LambdaFunctionDetails {
     }
 
     pub fn render(&self, f: &LambdaFunctionInfo) -> iced::Element<Message> {
-        column![
-            self.string_prop("Name", &f.0.function_name),
-            self.string_prop("Description", &f.0.description),
-            self.string_prop("ARN", &f.0.function_arn),
-            row![
-                self.string_prop("Runtime", &f.0.runtime.as_ref().map(|r| r.to_string())),
-                self.string_prop(
-                    "Architectures",
-                    &f.0.architectures.as_ref().map(|a| a
-                        .iter()
-                        .map(|a| a.to_string())
-                        .collect::<Vec<_>>()
-                        .join(", "))
-                )
-            ],
-            self.string_prop("Role", &f.0.role),
-            row![
-                self.string_prop("Handler", &f.0.handler),
-                self.string_prop(
-                    "Package type",
-                    &f.0.package_type.as_ref().map(|p| p.to_string())
-                ),
-            ],
-            row![
-                self.num_prop("Code size", &Some(f.0.code_size), Some("bytes")),
-                self.num_prop("Timeout", &f.0.timeout, Some("second(s)")),
-            ],
-            row![
-                self.num_prop("Memory size", &f.0.memory_size, Some("MB")),
-                self.num_prop(
-                    "Ephemeral storage",
-                    &f.0.ephemeral_storage.as_ref().map(|s| s.size),
-                    Some("MB"),
-                ),
-            ],
-            self.string_prop("Last modified", &f.0.last_modified),
-            self.string_prop("Code SHA256", &f.0.code_sha256),
-            self.string_prop("Version", &f.0.version),
-            self.render_vpc_config(f),
-            self.string_prop("KMS Key ARN", &f.0.kms_key_arn),
-            self.string_prop("Master ARN", &f.0.master_arn),
-            self.string_prop("Revision ID", &f.0.revision_id),
-            //self.string_prop("State", &f.0.state.as_ref().map(|s| s.to_string())),
-            //self.string_prop("State reason", &f.0.state_reason),
-            //self.string_prop(
-            //    "State reason code",
-            //    &f.0.state_reason_code.as_ref().map(|c| c.to_string())
-            //),
-            //self.string_prop(
-            //    "Last update status",
-            //    &f.0.last_update_status.as_ref().map(|s| s.to_string())
-            //),
-            //self.string_prop("Last update status reason", &f.0.last_update_status_reason),
-            //self.string_prop(
-            //    "Last update status reason code",
-            //    &f.0.last_update_status_reason_code
-            //        .as_ref()
-            //        .map(|c| c.to_string())
-            //),
-            //self.string_prop(
-            //    "Signing profile version ARN",
-            //    &f.0.signing_profile_version_arn
-            //),
-            //self.string_prop("Signing job ARN", &f.0.signing_job_arn),
-            self.render_environment_variables(f),
-        ]
+        scrollable(
+            column![
+                self.string_prop("Name", &f.0.function_name),
+                self.string_prop("Description", &f.0.description),
+                self.string_prop("ARN", &f.0.function_arn),
+                self.string_prop("Role", &f.0.role),
+                row![
+                    self.string_prop("Handler", &f.0.handler),
+                    self.string_prop("Runtime", &f.0.runtime.as_ref().map(|r| r.to_string())),
+                    self.string_prop(
+                        "Architectures",
+                        &f.0.architectures.as_ref().map(|a| a
+                            .iter()
+                            .map(|a| a.to_string())
+                            .collect::<Vec<_>>()
+                            .join(", "))
+                    ),
+                    self.string_prop(
+                        "Package type",
+                        &f.0.package_type.as_ref().map(|p| p.to_string())
+                    ),
+                ]
+                .spacing(8),
+                row![
+                    self.num_prop("Code size", &Some(f.0.code_size), Some("bytes")),
+                    self.num_prop("Timeout", &f.0.timeout, Some("second(s)")),
+                    self.num_prop("Memory size", &f.0.memory_size, Some("MB")),
+                    self.num_prop(
+                        "Ephemeral storage",
+                        &f.0.ephemeral_storage.as_ref().map(|s| s.size),
+                        Some("MB"),
+                    ),
+                ]
+                .spacing(8),
+                self.string_prop("Last modified", &f.0.last_modified),
+                self.string_prop("Code SHA256", &f.0.code_sha256),
+                self.string_prop("Version", &f.0.version),
+                self.render_vpc_config(f),
+                self.string_prop("KMS Key ARN", &f.0.kms_key_arn),
+                self.string_prop("Master ARN", &f.0.master_arn),
+                self.string_prop("Revision ID", &f.0.revision_id),
+                //self.string_prop("State", &f.0.state.as_ref().map(|s| s.to_string())),
+                //self.string_prop("State reason", &f.0.state_reason),
+                //self.string_prop(
+                //    "State reason code",
+                //    &f.0.state_reason_code.as_ref().map(|c| c.to_string())
+                //),
+                //self.string_prop(
+                //    "Last update status",
+                //    &f.0.last_update_status.as_ref().map(|s| s.to_string())
+                //),
+                //self.string_prop("Last update status reason", &f.0.last_update_status_reason),
+                //self.string_prop(
+                //    "Last update status reason code",
+                //    &f.0.last_update_status_reason_code
+                //        .as_ref()
+                //        .map(|c| c.to_string())
+                //),
+                //self.string_prop(
+                //    "Signing profile version ARN",
+                //    &f.0.signing_profile_version_arn
+                //),
+                //self.string_prop("Signing job ARN", &f.0.signing_job_arn),
+                self.render_environment_variables(f),
+            ]
+            .padding(4)
+            .spacing(8),
+        )
         .into()
     }
 
@@ -146,12 +150,15 @@ impl LambdaFunctionDetails {
                 row![
                     self.string_prop("VPC ID", &None),
                     self.string_prop("Subnet IDs", &None),
-                ],
+                ]
+                .spacing(8),
                 row![
                     self.string_prop("Security group IDs", &None),
                     self.bool_prop("IPv6 allowed for dual stack", &None),
                 ]
+                .spacing(8)
             ]
+            .spacing(8)
             .into();
         };
 
@@ -163,7 +170,8 @@ impl LambdaFunctionDetails {
             row![
                 self.string_prop("VPC ID", vpc_id),
                 self.string_prop("Subnet IDs", &subnet_ids),
-            ],
+            ]
+            .spacing(8),
             row![
                 self.string_prop("Security group IDs", &security_group_ids),
                 self.bool_prop(
@@ -171,15 +179,14 @@ impl LambdaFunctionDetails {
                     &vpc_config.ipv6_allowed_for_dual_stack
                 ),
             ]
+            .spacing(8)
         ]
+        .spacing(8)
         .into()
     }
 
     fn render_environment_variables(&self, f: &LambdaFunctionInfo) -> Element<Message> {
-        let mut c = column![text("Environment variables: ")
-            .width(Length::Fixed(200.0))
-            .align_x(iced::alignment::Horizontal::Right)
-            .align_y(iced::alignment::Vertical::Center)];
+        let mut c = column![self.render_label("Environment variables: ")];
 
         let Some(env) = f.0.environment.as_ref() else {
             return c.into();
@@ -208,19 +215,11 @@ impl LambdaFunctionDetails {
 
     fn string_prop(&self, label: &str, value: &Option<String>) -> Element<Message> {
         let value = match value {
-            Some(v) => text_input("", &v.clone()).on_input(do_nothing),
-            None => text_input("", "-"),
+            Some(v) => text_input("", v).width(Length::Fill).on_input(do_nothing),
+            None => text_input("-", ""),
         };
-        row![
-            text(format!("{}: ", label))
-                .width(Length::Fixed(200.0))
-                .height(Length::Fixed(32.0))
-                .align_x(iced::alignment::Horizontal::Right)
-                .align_y(iced::alignment::Vertical::Center),
-            value,
-        ]
-        .spacing(4)
-        .into()
+
+        column![self.render_label(label), value].into()
     }
 
     fn num_prop(
@@ -233,36 +232,37 @@ impl LambdaFunctionDetails {
             Some(v) => text_input("", &v.to_string()).on_input(do_nothing),
             None => text_input("", "-"),
         };
-        row![
-            text(format!("{}: ", label))
-                .width(Length::Fixed(200.0))
-                .height(Length::Fixed(32.0))
-                .align_x(iced::alignment::Horizontal::Right)
-                .align_y(iced::alignment::Vertical::Center),
-            value,
-            text(unit.unwrap_or("").to_string())
-                .height(Length::Fixed(32.0))
-                .align_y(iced::alignment::Vertical::Center),
-        ]
-        .spacing(4)
-        .into()
+
+        let unit = text(unit.unwrap_or("").to_string())
+            .height(Length::Fixed(32.0))
+            .align_y(iced::alignment::Vertical::Center);
+
+        column![self.render_label(label), row![value, unit].spacing(4)]
+            .spacing(4)
+            .into()
     }
 
     fn bool_prop(&self, label: &str, value: &Option<bool>) -> Element<Message> {
         let value = match value {
-            Some(v) => checkbox(label, *v).on_toggle(do_nothing_bool),
-            None => {
-                checkbox(label, false).on_toggle_maybe(false.then_some(Message::DoNothingOnToggle))
-            }
+            Some(v) => text_input("", if *v { "true" } else { "false" }).on_input(do_nothing),
+            None => text_input("-", ""),
         };
-        row![value.width(Length::Fill)].spacing(4).into()
+        column![self.render_label(label), value.width(Length::Fill)]
+            .spacing(4)
+            .into()
+    }
+
+    fn render_label(&self, label: &str) -> Element<Message> {
+        text(label.to_string())
+            .font(fonts::get_bold_font())
+            .width(Length::Fill)
+            .size(10.0)
+            .align_x(Horizontal::Left)
+            .align_y(Vertical::Center)
+            .into()
     }
 }
 
 fn do_nothing(_: String) -> Message {
-    Message::DoNothing
-}
-
-fn do_nothing_bool(_: bool) -> Message {
     Message::DoNothing
 }
